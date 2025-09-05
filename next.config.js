caa39547f28a73aa659747a27e05b5bf46d63a67
Dheeraj Vemula <vemula.dheeraj@gmail.com>
2025-09-05 15:21:05 +0530

    new changes

--- next.config.js
+++ next.config.js
@@ -2,14 +2,16 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const isUserOrOrgSite = /\.github\.io$/i.test(repoName);
 
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd && repoName ? `/${repoName}/` : '',
-  basePath: isProd && repoName ? `/${repoName}` : '',
+  // For user/organization pages (e.g. username.github.io), basePath should be empty
+  assetPrefix: isProd && repoName && !isUserOrOrgSite ? `/${repoName}/` : '',
+  basePath: isProd && repoName && !isUserOrOrgSite ? `/${repoName}` : '',
 };
 
 module.exports = nextConfig;
